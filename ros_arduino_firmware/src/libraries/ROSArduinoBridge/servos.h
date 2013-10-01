153f947a39f81733f998240c71f249ff431117d1
karl <karl@karl-ros.(none)>
2013-10-01 13:10:54 +0300

    Adding ros firmware

---
+++ ros_arduino_firmware/src/libraries/ROSArduinoBridge/servos.h
@@ -0,0 +1,9 @@
+/* Define the attachment of any servos here.
+   The example shows two servos attached on pins 9 and 10.
+*/
+
+#define N_SERVOS 2
+
+Servo servos [N_SERVOS];
+byte servoPins [N_SERVOS] = {10, 11};
+
